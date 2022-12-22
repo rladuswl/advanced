85687eaa031c42e604afe85c564720b08f8f85db
kyj <khkkgon0118@gmail.com>
2022-12-22 16:05:50 +0900

    필드 동기화 - 적용

---
+++ src/main/java/study/advanced/LogTraceConfig.java
@@ -0,0 +1,15 @@
+package study.advanced;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import study.advanced.trace.logtrace.FieldLogTrace;
+import study.advanced.trace.logtrace.LogTrace;
+
+@Configuration
+public class LogTraceConfig {
+
+    @Bean
+    public LogTrace logTrace() {
+        return new FieldLogTrace();
+    }
+}
