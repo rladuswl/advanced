ff17f45bf84704fead4f8b4ba89083a73abafb76
kyj <khkkgon0118@gmail.com>
2022-12-28 00:46:10 +0900

    템플릿 콜백 패턴 - 적용

---
+++ src/main/java/study/advanced/app/v5/OrderControllerV5.java
@@ -0,0 +1,32 @@
+package study.advanced.app.v5;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import study.advanced.trace.callback.TraceCallback;
+import study.advanced.trace.callback.TraceTemplate;
+import study.advanced.trace.logtrace.LogTrace;
+
+@RestController // @Controller + @ResponseBody
+public class OrderControllerV5 {
+
+    private final OrderServiceV5 orderService;
+    private final TraceTemplate template;
+
+    @Autowired // 생략 가능
+    public OrderControllerV5(OrderServiceV5 orderService, LogTrace trace) {
+        this.orderService = orderService;
+        this.template = new TraceTemplate(trace);
+    }
+
+    @GetMapping("/v5/request")
+    public String request(String itemId) {
+        return template.execute("OrderController.request()", new TraceCallback<>() {
+            @Override
+            public String call() {
+                orderService.orderItem(itemId);
+                return "ok";
+            }
+        });
+    }
+}
